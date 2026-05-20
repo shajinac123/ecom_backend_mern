e8401c978f78903d179f977cd74f4b88797fd25c
naflaa1 <saleemac111@gmail.com>
2026-05-20 09:52:06 +0530

    payment page

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -7,6 +7,24 @@ import axios from "axios";
 
 
 
+export const getSingleOrder = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+      return res.status(404).json({
+        message: "Order not found",
+      });
+    }
+
+    res.json(order);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
 export const PostOrder = async (req, res) => {
   try {
     // Auth check
@@ -125,7 +143,7 @@ export const PostOrder = async (req, res) => {
 
         order_meta: {
           return_url:
-            "http://localhost:3000/payment-success?order_id={order_id}",
+            "http://54.196.231.231/orderdetails?order_id={order_id}",
         },
       },
       {
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { PostOrder,GetOrder,UpdateOrderStatus} from "../controller/order.controller.js"
+import { PostOrder,GetOrder,UpdateOrderStatus,getSingleOrder} from "../controller/order.controller.js"
 import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post("/api/order",authMiddleware, PostOrder)
 router.get("/api/order",authMiddleware,GetOrder)
 router.put("/api/order/:id",authMiddleware,UpdateOrderStatus)
+router.get("/api/order/:id", authMiddleware, getSingleOrder);
 
 export default router
 
